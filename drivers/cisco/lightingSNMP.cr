4c5bf3c81121943998517771ecdfe18ea1df68cd
MarioFavalliR <mario@place.technology>
2023-03-01 15:51:32 +0000

    lighting snmp updated TEST3

--- drivers/cisco/lightingSNMP.cr
+++ drivers/cisco/lightingSNMP.cr
@@ -25,11 +25,19 @@ class LightingSNMP::Driver < PlaceOS::Driver
         session = SNMP::Session.new
         socket.write_bytes session.set("1.3.6.1.2.1.105.1.1.1.3", 2)
         socket.flush   
-        response = session.parse(socket.read_bytes(ASN1::BER))
-        self["state"] = response.value.get_string 
+        #response = session.parse(socket.read_bytes(ASN1::BER))
+        self["state"] = "on"
     end
 
     def off()
-    
+        socket = UDPSocket.new
+        socket.connect("192.168.20.253", 2)
+        socket.sync = false
+        socket.read_timeout = 3
+        session = SNMP::Session.new
+        socket.write_bytes session.set("1.3.6.1.2.1.105.1.1.1.3", 1)
+        socket.flush   
+        #response = session.parse(socket.read_bytes(ASN1::BER))
+        self["state"] = "off"
     end
 end
